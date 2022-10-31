a8356180590882d0f946ebb0e9e4a4d99ccf322d
BelleZarocha <sarocha.pj@gmail.com>
2022-10-31 13:35:44 +0700

    init

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing", {
+    strings: ["Front-End Developer", "UI Designer", "Digital Marketer", "Content Creater"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true
+});
